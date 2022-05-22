5869cb1c78b96fb5cfc2dafd6679db947ace2b20
Ajay <ajay@Ajays-MacBook-Air.local>
2022-05-22 21:01:12 +0800

    add basic error handling

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+return [
+    'desktop_screenshots' => [
+        'sourceDir' => '/Users/ajay/Desktop', 
+        'targetDir' => '/Users/ajay/Desktop/screenshot',
+        'needle' => 'screenshot'
+    ], 
+    // 'download_dmgs' => [
+    //     'sourceDir' => '/Users/ajay/Downloads',
+    //     'targetDir' => '/Users/ajay/Downloads/.dmg',
+    //     'needle' => '.dmg'
+    // ]
+]; 
--- index.php
+++ index.php
@@ -4,8 +4,46 @@ require __DIR__ . '/vendor/autoload.php';
 
 use App\MoveFilesToTarget;
 
-MoveFilesToTarget::handle(
-    "/Users/ajay/Desktop", 
-    "/Users/ajay/Desktop/Screenshots",
-    "screenshot"
-); 
+$config = require 'config.php'; 
+
+// var_dump($config); 
+
+if (!count($config)) {
+    exit("No config values provided. Add a config value to continue"); 
+}
+
+foreach ($config as $name => $values) 
+{
+    if (!isset($values['sourceDir'])) 
+    {
+        exit("The source dir for $name is blank. Add a value"); 
+    }
+
+    if (!isset($values['targetDir'])) {
+        exit("The target dir for $name is blank. Add a value");
+    }
+
+    $targetDirFolderName = collect(
+        explode('/', rtrim($values['targetDir'], '/'))
+    )->reverse()
+    ->first(); 
+
+    if ($targetDirFolderName == $values['needle']) {
+        exit("The target dir's folder name for $name needs to be different from the corresponding needle value " . $values['needle']); 
+    }
+}
+
+foreach ($config as $name => $values) 
+{
+    MoveFilesToTarget::handle(
+        $values['sourceDir'],
+        $values['targetDir'],
+        $values['needle']
+    );
+}
+
+// MoveFilesToTarget::handle(
+//     "/Users/ajay/Desktop", 
+//     "/Users/ajay/Desktop/Screenshots",
+//     "screenshot", 
+// ); 
